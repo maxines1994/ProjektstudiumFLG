aa22ede37606ae330f474b13a273164282e11371
PC <judikator89@web.de>
2020-10-22 12:52:27 +0200

    GLOT-45 Statusanlage nur als Konstanten ermöglichen
    Status werden nur noch in constants.py angelegt. Diese werden automatisch in die Status-Tabelle migriert.

---
+++ gtserver/gtapp/constants.py
@@ -0,0 +1,15 @@
+#All global constants will be defined in this file
+#This is only used for statuses at the moment.
+#If you change any statuses in this file, you must not add new migrations for the added statuses
+#These will be added automatically with the next migration
+
+#Article Status
+ARTICLE_ACTIVE          = 'A'
+ARTICLE_INACTIVE        = 'I'
+
+#Todo Status
+TODO_DONE               = 'E'
+TODO_TO_BE_DONE         = 'D'
+
+#CustOrderDet Status
+CUSTORDERDET_TO_BE_DONE = 'D'
