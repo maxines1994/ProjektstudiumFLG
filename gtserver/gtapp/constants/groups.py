68314ddaef585d495324ec04694ae6ec9ffaef43
PC <judikator89@web.de>
2020-10-23 18:09:43 +0200

    GLOT-45
    Neue Konstanten angelegt, und sie in eigenen Ordner ausgelagert

---
+++ gtserver/gtapp/constants/groups.py
@@ -0,0 +1,21 @@
+#All global group-constants will be defined in this file.
+
+LEITUNGSTEAM                        = 'Leitungsteam'
+KUNDEN                              = 'Kunden'
+KUNDENDIENST                        = 'Kundendienst'
+INTERNE_DIENSTLEISTUNG              = 'Interne Dienstleistung'
+PRODUKTIONSDIENSTLEISTUNG           = 'Produktionsdienstleistung'
+PRODUKTION                          = 'Produktion'
+LIEFERANTEN                         = 'Lieferanten'
+SPIELLEITUNG                        = 'Spielleitung'
+
+#Abbreviations of the groups
+
+LEITUNGSTEAM_CODE                   = 'LT'
+KUNDEN_CODE                         = 'K'
+KUNDENDIENST_CODE                   = 'KD'
+INTERNE_DIENSTLEISTUNG_CODE         = 'IDL'
+PRODUKTIONSDIENSTLEISTUNG_CODE      = 'PDL'
+PRODUKTION_CODE                     = 'PRO'
+LIEFERANTEN_CODE                    = 'L'
+SPIELLEITUNG_CODE                   = 'SL'
