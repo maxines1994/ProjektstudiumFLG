b546e24e8f620c7d869be7e97add6ce33c72510f
eckende <jan@d-h-e.de>
2020-12-11 00:02:07 +0100

    Make sure changing users only works if debug flag is set

--- gtserver/gtapp/views/authViews.py
+++ gtserver/gtapp/views/authViews.py
@@ -2,6 +2,8 @@ from gtapp.utils import get_context
 from django.contrib.auth import get_user_model, authenticate, login
 from django.shortcuts import render, redirect
 from django.http import HttpResponse 
+from django.conf import settings
+
 
 def change_user_view(request):
     c = get_context("Benutzer wechseln","Benutzer wechseln")
@@ -9,7 +11,7 @@ def change_user_view(request):
     return render(request, "registration/change_user.html", c)
 
 def change_user_to_view(request, id):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated and settings.DEBUG:
         login(request, get_user_model().objects.get(id=id))
         return redirect('/')
     else:
