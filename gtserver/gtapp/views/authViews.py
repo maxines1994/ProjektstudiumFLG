d5ab43117736315bac3222d27419d6c3366a6548
eckende <jan@d-h-e.de>
2020-12-10 23:42:14 +0100

    Include auth urls/views

---
+++ gtserver/gtapp/views/authViews.py
@@ -0,0 +1,16 @@
+from gtapp.utils import get_context
+from django.contrib.auth import get_user_model, authenticate, login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse 
+
+def change_user_view(request):
+    c = get_context("Benutzer wechseln","Benutzer wechseln")
+    c['users'] = get_user_model().objects.all()
+    return render(request, "registration/change_user.html", c)
+
+def change_user_to_view(request, id):
+    if request.user.is_authenticated:
+        login(request, get_user_model().objects.get(id=id))
+        return redirect('/')
+    else:
+        return HttpResponse('Unauthorized', status=401)
