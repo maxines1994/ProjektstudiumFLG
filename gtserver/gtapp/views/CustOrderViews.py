2d86c32379491628a0c512ecdd47d9313571ab84
Luke <BlankemeyerL@web.de>
2020-12-20 21:59:14 +0100

    Comments

--- gtserver/gtapp/views/CustOrderViews.py
+++ gtserver/gtapp/views/CustOrderViews.py
@@ -8,30 +8,34 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import PermissionRequiredMixin
 
 
-# CustOrder von Joga und Bestellungen der 
+# CustOrder von Joga und Bestellungen der Kunden
 class Cust_order_create_view(CreateView):
     form_class = Cust_order_form
     template_name = "CustOrderForm.html"
 
+    # Umleitung auf die Alter View
     def form_valid(self, form):
         new_cust_order = form.save()
         return HttpResponseRedirect("/cust_order/alter/" + str(new_cust_order.pk) + "/")
     
+    # Navbar Context
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context = get_context_back(context,"Auftrag erstellen","Aufträge")
         return context
 
-
+# CustOrder von Joga und Bestellungen der Kunden
 class Cust_order_alter_view(UpdateView):
     form_class = Cust_order_form
     template_name = "CustOrderForm.html"
     success_url = "/cust_order/"
 
+    # Objekt für Alter view getten
     def get_object(self, queryset=None):
         obj = CustOrder.objects.get(id=self.kwargs['id'])
         return obj
 
+    # Context zum Templating hinzufügen 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['items'] = CustOrderDet.objects.filter(cust_order=self.get_object().pk)
@@ -39,6 +43,7 @@ class Cust_order_alter_view(UpdateView):
         context = get_context_back(context,"Auftrag ändern","Aufträge")
         return context
 
+# CustOrder von Joga und Bestellungen der Kunden
 class Cust_order_delete_view(DeleteView):
     template_name = "delete.html"
 
@@ -52,6 +57,7 @@ class Cust_order_delete_view(DeleteView):
         self.object.delete()
         return HttpResponseRedirect(success_url)
 
+# CustOrder von Joga und Bestellungen der Kunden
 class Cust_order_det_create_view(PermissionRequiredMixin,CreateView):
     permission_required = 'gtapp.create_cust_order'
     form_class = Cust_order_det_form
@@ -66,7 +72,8 @@ class Cust_order_det_create_view(PermissionRequiredMixin,CreateView):
         form.instance.cust_order = CustOrder.objects.get(id=self.kwargs["cust_order"])
         form.save()
         return HttpResponseRedirect("/cust_order/alter/" + str(self.kwargs["cust_order"]) + "/")
-    
+
+# CustOrder von Joga und Bestellungen der Kunden
 class Cust_order_det_alter_view(UpdateView):
     form_class = Cust_order_det_form
     template_name = "CustOrderForm.html"
@@ -84,6 +91,7 @@ class Cust_order_det_alter_view(UpdateView):
         form.save()
         return HttpResponseRedirect("/cust_order/alter/" + str(self.kwargs["id"]) + "/")
 
+# CustOrder von Joga und Bestellungen der Kunden
 class Cust_order_det_delete_view(DeleteView):
     template_name = "delete.html"
 
@@ -96,7 +104,8 @@ class Cust_order_det_delete_view(DeleteView):
         success_url = "/cust_order/alter/" + str(self.object.cust_order.pk) + "/"
         self.object.delete()
         return HttpResponseRedirect(success_url)
-    
+
+# CustOrder von Joga und Bestellungen der Kunden
 class Cust_order_view(TemplateView):
     template_name = "CustOrder.html"
 
@@ -105,4 +114,3 @@ class Cust_order_view(TemplateView):
         context['orders'] = CustOrder.objects.all()
         context = get_context_back(context,"Auftragsliste","Aufträge")
         return context
-
--- gtserver/gtapp/views/stdViews.py
+++ gtserver/gtapp/views/stdViews.py
@@ -9,20 +9,24 @@ from gtapp.constants import *
 
 # Anlegen von Views mit dictionary TITEL und Markierung für den User wo er sich gerade befindet.
 
+# Startseite
 def home_view(request):
     c = get_context("Startseite","Startseite")
     return render(request, "home.html", c)
 
+# Task Schaltfläche
 def tasks_view(request):
     c = get_context("Aufgaben","Aufgaben")
     return render(request, "tasks.html", c)
 
+# Zugewiesene Tasks View
 def tasks_list_assigned_view(request):
     c = get_context("Zugewiesene Aufgaben","Aufgaben")
     c['tasks'] = Todo.objects.filter(user=request.user)
     c['Headline'] = "Mir zugewiesene Aufgaben"
     return render(request, "tasks_list.html", c)
 
+# Nicht-zugewiesene Tasks View
 def tasks_list_notassigned_view(request):
     c = get_context("Zugewiesene Aufgaben","Aufgaben")
     myList = list()
@@ -34,18 +38,20 @@ def tasks_list_notassigned_view(request):
     c['Headline'] = "Verfügbare Aufgaben"
     return render(request, "tasks_list.html", c)
 
+# Weise Task User zu
 def tasks_assign_to_me_view(request, **kwargs):
     Todo.objects.filter(pk=kwargs["id"]).update(user=request.user)
     return HttpResponseRedirect(reverse("tasks_assigned"))
 
+# Weise Task Team zu
 def tasks_share_to_team_view(request, **kwargs):
     Todo.objects.filter(pk=kwargs["id"]).update(user_id=UNKNOWN)
     return HttpResponseRedirect(reverse("tasks_assigned"))
 
+# Task Detail View
 class Tasks_detail_view(DetailView):
     template_name = "tasks_detail.html"
     model = Todo
-
 
 class Tasks_list_assigned_view(TemplateView):
     pass
