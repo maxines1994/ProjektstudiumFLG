fcc5c17ce0e5c519a8f76fc9742d6a8019400ae0
PC <judikator89@web.de>
2021-02-01 13:35:45 +0100

    Metadaten werden automatisch gespeichert
    Paket "crum" muss installiert sein.

--- gtserver/gtapp/models/gtmodel.py
+++ gtserver/gtapp/models/gtmodel.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from gtapp.constants import *
 from django.contrib import admin
+from crum import get_current_user
+from .timer import Timers
 
 class GtModel(models.Model):
     """
@@ -10,18 +12,26 @@ class GtModel(models.Model):
     Methoden save() zum Speichern und create() zum Anlegen von Datensaetzen. 
     """
     _creation_date = models.DateTimeField(auto_now_add=True)
+    _creation_gameday = models.SmallIntegerField(auto_created=True, default=0)
     _creation_user = models.ForeignKey(User, related_name='%(class)s_creation_user',null=True, on_delete=models.SET_NULL)
     _update_date = models.DateTimeField(auto_now=True)
+    _update_gameday = models.SmallIntegerField(null=True)
     _update_user = models.ForeignKey(User, related_name='%(class)s_update_user',null=True, on_delete=models.SET_NULL)
 
     class Meta:
         abstract = True
 
     def save(self, *args, **kwargs):
-        #Hier muss noch eine Methode gefunden werden um an dieser Stelle den User zu holen, der diese Methode aufruft,
-        #optimalerweise ohne ihn explizit uebergeben zu muessen
-       # self._update_user = user
+        """
+        Speichert den aktuellen User und Spieltag in _creation_user/_update_user bzw. _creation_gameday/_update_gameday
+        """
+        user = get_current_user()
+        gameday = Timers.get_current_day()
+        #Ist noch kein Primaerschluessel vergeben, wird der Datensatz erstmalig angelegt
+        if not self.pk:
+            self._creation_user = user
+            self._creation_gameday = gameday
+
+        self._update_user = user
+        self._update_gameday = gameday
         super().save(*args, **kwargs)
-
-
-    #Hier fehlen noch create()-Methoden, die den Standard ueberschreiben
