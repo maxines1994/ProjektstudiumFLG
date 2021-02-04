8e7cb3aaa0eda171d9d41e945eea0f4f8a9a6487
Sebastian R <DerKommentatorHD@gmail.com>
2021-02-04 21:20:06 +0100

    Reparatur in Anpassung umbenannt

--- gtserver/gtapp/models/custcomplaint.py
+++ gtserver/gtapp/models/custcomplaint.py
@@ -13,8 +13,8 @@ class CustComplaint(Complaint):
         ERFASST                         = '1', ('Erfasst')
         IN_REKLAMATION                  = '2', ('In Reklamation') ##Nur für den Kunden
         REKLAMATION_FREIGEGEBEN         = '3', ('Reklamation freigegeben')
-        IN_REPARATUR                    = '4', ('In Reparatur')
-        REPARATUR_ABGESCHLOSSEN         = '5', ('Reparatur abgeschlossen')
+        IN_ANPASSUNG                    = '4', ('In Anpassung')
+        ANPASSUNG_ABGESCHLOSSEN         = '5', ('Anpassung abgeschlossen')
         LIEFERUNG_AN_KUNDENDIENST       = '6', ('Lieferung an Kundendienst')
         LIEFERUNG_AN_KUNDE              = '7', ('Lieferung an Kunde')
         GELIEFERT                       = '8', ('Geliefert')
