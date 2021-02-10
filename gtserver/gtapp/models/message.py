eebdf82b6cd7453a30ce1b69f5642e35439a01e8
PC <judikator89@web.de>
2021-02-10 16:27:35 +0100

    Nachrichten: Zeitstempel wann Nachricht gelesen wurde

--- gtserver/gtapp/models/message.py
+++ gtserver/gtapp/models/message.py
@@ -18,6 +18,7 @@ class Message(GtModel):
     text = models.TextField()
     subject = models.CharField(max_length=100)
     sent_on = models.SmallIntegerField()
+    read_on = models.SmallIntegerField()
     sender =  models.ForeignKey(User, default=UNKNOWN, related_name='sender', on_delete=models.SET_DEFAULT)
     receiver = models.ForeignKey(Group, default=UNKNOWN, related_name='receiver', on_delete=models.SET_DEFAULT)
 
