246c0e1c56fd62b7303f7dc9c604e1b2e6b06f66
eckende <jan@d-h-e.de>
2021-02-10 16:19:52 +0100

    Fix View Imports

--- gtserver/gtapp/views/__init__.py
+++ gtserver/gtapp/views/__init__.py
@@ -13,8 +13,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 import json
 
 # Reihenfolge alphabetisch / wie Dateibaum
-from .ApiViews import *
-from .AuthViews import *
+from .apiViews import *
+from .authViews import *
 from .BoxViews import *
 from .cancelViews import *
 from .CustComplaintViews import *
