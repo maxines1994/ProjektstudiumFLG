9ecf354fea382c3548155476eebff5d08fad2ef6
Sebastian R <DerKommentatorHD@gmail.com>
2021-02-11 21:15:50 +0100

    The start of something great - actually working suppcomplaints AMA

--- gtserver/gtapp/models/suppcomplaintdet.py
+++ gtserver/gtapp/models/suppcomplaintdet.py
@@ -10,5 +10,22 @@ class SuppComplaintDet(ComplaintDet):
     quantity = models.PositiveSmallIntegerField()
     redelivery = models.BooleanField(default=False)
 
+    class Status(models.TextChoices):
+
+        STANDARD                        = '0', ('Standard')
+        ERFASST                         = '1', ('Erfasst')
+        BEI_PDL                         = '2', ('Bei PDL') ##Nur wenn Rekla in PRO erstellt
+        AUS_LAGER_LIEFERN               = '3', ('Aus Lager geliefert')
+        NEU_BESTELLT                    = '4', ('Neu bestellt')
+        BESTANDSPRUEFUNG_AUSSTEHEND     = '5', ('Bestandsprüfung ausstehend')
+        GELIEFERT                       = '6', ('Geliefert')
+        ABGESCHLOSSEN                   = '7', ('Abgeschlossen') ##Nur wenn keine Neulieferung
+
+    status = models.CharField(
+        max_length = 1,
+        choices = Status.choices,
+        default = Status.ERFASST,
+    )
+
     def __str__(self):
         return str(self.supp_order_det.part.description)
