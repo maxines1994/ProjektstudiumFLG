0ca843895fcb338b44c43bd28f7db312a708b391
PC <judikator89@web.de>
2021-02-14 17:52:36 +0100

    TaskType-SQL sortiert nach Gruppe

--- gtserver/gtapp/sqls/task_types_by_group.sql
+++ gtserver/gtapp/sqls/task_types_by_group.sql
@@ -1,5 +1,5 @@
 --Auswertung: TaskTypes sortiert nach Gruppe
-SELECT g.name Gruppe, t.id TaskTypeID, t.title Titel, t.description Beschreibung, t.status_model 'Model für Status', t.task_model 'Model für Task', t.view_url 'Redirect-Template', t.template_kwargs 'Kwargs für Redirect'
+SELECT g.name Gruppe, t.id TaskTypeID, t.title Titel, t.description Beschreibung, t.status_model 'Model für Status', t.status Status, t.task_model 'Model für Task', t.view_url 'Redirect-Template', t.view_kwargs_id 'Kwargs für Redirect', t.for_all_details
 FROM gtapp_tasktype t, auth_group g
 WHERE t.group_id = g.id
 ORDER BY Gruppe
