17fead0e492b2ab44478760a01605ac67690fab5
PC <judikator89@web.de>
2021-02-16 21:28:41 +0100

    If-Verzweigung gefixt

--- gtserver/gtapp/models/timer.py
+++ gtserver/gtapp/models/timer.py
@@ -10,17 +10,19 @@ class Timers(models.Model):
     def get_current_day(cls):
         sum = 0
         nowtime = datetime.datetime.now()
+        # Erst reinspringen, wenn diese Tabelle bereits existiert, sonst knallts bei den makemigrations,
+        # weil Tasks ein get_current_day aufruft
+        if Timers._meta.db_table in connection.introspection.table_names():
+            myList = list(Timers.objects.all().order_by('timestamp'))
+            for i in myList:
+                if i.nowactive:
+                    sum -= i.timestamp.timestamp()
+                else:
+                    sum += i.timestamp.timestamp()
+            
 
-        myList = list(Timers.objects.all().order_by('timestamp'))
-        for i in myList:
-            if i.nowactive:
-                sum -= i.timestamp.timestamp()
-            else:
-                sum += i.timestamp.timestamp()
-        
-
-        if len(myList) != 0:
-            if myList[-1].nowactive:
-                sum += nowtime.timestamp()
+            if len(myList) != 0:
+                if myList[-1].nowactive:
+                    sum += nowtime.timestamp()
 
         return int(sum/180+1)
